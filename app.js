8cdfa2ea57bb0c5349a25aa0cdf9969f6e7f29c4
bimeiras <mbmeira31@gmail.com>
2021-07-11 19:01:11 -0300

    Level 2 e 3 completed.

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-
+var session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -17,12 +16,26 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useUnifiedTopology: true,  useNewUrlParser: true });
+
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,37 +51,35 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register", function (req, res) {
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("login");
+    }
+});
 
-    newUser.save(function(err) {
+app.post("/register", function (req, res) {
+    
+    User.register({username: req.body.username}, req.body.password, function (err, user) {
         if (err) {
-            res.send(err);
+            console.log(err);
+            res.redirect("/register");
         } else {
-            res.render("secrets");
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
         }
     });
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = md5(req.body.password);
-
-    User.findOne({email: username}, function (err, foundUser) {
-        if (err) {
-            console.log(err)
-        } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }
-        }
+    const user = new User ({
+        username: req.body.username,
+        password: req.body.password
     });
-})
+
+});
 
 
 app.listen(3000, function() {
